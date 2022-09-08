a667f2150ba3aaa1bbc76cd218bc2a7326ffc1e1
pbrosi <peter.brosi@gmail.com>
2022-09-08 13:33:25 +0200

    modified Employee

--- CopyEmployeePackageToVM.sh
+++ CopyEmployeePackageToVM.sh
@@ -1,1 +1,1 @@
-cp -rf /var/lib/jenkins/workspace/AccelerateMSRCustomerImage/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
+cp -rf /var/lib/jenkins/workspace/CreateEmployeeImage/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages

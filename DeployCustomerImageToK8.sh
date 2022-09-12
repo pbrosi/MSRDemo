513f2134098d31c4d882f3ec8d28e07f7b0c722d
pbrosi <peter.brosi@gmail.com>
2022-09-12 18:04:52 +0200

    added delete

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,4 +2,10 @@
 #
 
 yes | /usr/local/bin/rancher login ${RANCHER_URL} --token ${RANCHER_TOKEN}
+/usr/local/bin/rancher kubectl delete ingress msr-ingress
+/usr/local/bin/rancher kubectl delete svc msr-service
+/usr/local/bin/rancher kubectl delete deployment msr-deployment
+
+echo "Deleted"
+
 /usr/local/bin/rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 

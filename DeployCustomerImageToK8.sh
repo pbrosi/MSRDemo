21e4555365fb1b9774f8c2c117991d09aa71ddb7
pbrosi <peter.brosi@gmail.com>
2022-09-12 11:21:41 +0200

    modified rancher login

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
 
-cd /opt/rancher
-./rancher login https://rancher.eur.ad.sag/v3 --token ${RANCHER_TOKEN}
+rancher login ${RANCHER_URL} --token ${RANCHER_TOKEN}
 $1
-./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
+rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 

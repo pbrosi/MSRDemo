c107a58fb5bbf909a8e54b993deb4345906f1362
pbrosi <peter.brosi@gmail.com>
2022-09-12 17:03:20 +0200

    modify yaml

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #
 
-rancher login ${RANCHER_URL} --token ${RANCHER_TOKEN}
-$1
+yes | rancher login ${RANCHER_URL} --token ${RANCHER_TOKEN}
 rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
